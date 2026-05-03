c85b9033cfefffb531e65f6875cd4e332b0e7ff3
AntonShirobokov <shirobokov.anton2016@gmail.com>
2026-05-03 19:10:55 +0400

    chore: update .env.example with required environment variables

--- src/main/java/com/luxus/tinterest/controller/RecommendationController.java
+++ src/main/java/com/luxus/tinterest/controller/RecommendationController.java
@@ -8,7 +8,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/v1")
+@RequestMapping("/v1/discovery")
 @RequiredArgsConstructor
 public class RecommendationController {
 
